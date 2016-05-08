529fb125e42de1a1e98d1cf5fb4ef7f2ce49a760
Rob Fazio <reemfz@gmail.com>
2016-05-07 19:38:46 -0700

    initial commit

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Rob Fazio on 5/4/16.
+//  Copyright © 2016 Rob Fazio. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
